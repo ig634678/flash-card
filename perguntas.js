d503b86bae677f22406e595814dffd66d2b86463
ig634678 <00001113220466SP@al.educacao.sp.gov.br>
2024-12-02 09:15:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de portugal?',
+    'A capital de Portugal é Lisboa'
+)
+
+criaCartao(
+    'Programação',
+    'escolha 1 tipo de linguagem de programação.',
+    'linguagens de projeto'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é bye'
+)
